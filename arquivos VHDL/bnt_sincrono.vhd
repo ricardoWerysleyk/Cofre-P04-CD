749c2b356dfb18949aad380a1250800e4605dcfa
Franklin Correia <franklin.correia.101@ufrn.edu.br>
2023-11-02 20:18:30 -0300

    atualizacao

--- arquivos VHDL/bnt_sincrono.vhd
+++ arquivos VHDL/bnt_sincrono.vhd
@@ -13,11 +13,12 @@ architecture ckt of bnt_sincrono is
                 q : out std_logic);
     end component;
 
-    signal S1, S2: std_logic;
+    signal S1, S2 notq: std_logic;
 
 begin
 
 FFD1: ffd port map(clk,'0', '0',BTN,S1);
-FFD2: ffd port map(clk,'0', '0', '1', S2);
+FFD2: ffd port map(clk,'0', '0', notq, S2);
 
+notq <= not(S1);
 saida <= S1 and S2;
