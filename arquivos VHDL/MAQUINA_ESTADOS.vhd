4cc6f72b2caeaa150f15a045ffd1294a46eaf80b
arleswasb <werbert.barradas.390@ufrn.edu.br>
2023-11-02 22:48:48 -0300

    MAQUINA_ESTADO

--- arquivos VHDL/MAQUINA_ESTADOS.vhd
+++ arquivos VHDL/MAQUINA_ESTADOS.vhd
@@ -3,31 +3,44 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity MAQUINA_ESTADOS is
    Port (clk, FAILURE, SUCCESS: in std_logic;
-		      E:  in std_logic_vector(2 downto 0);
-         	S: out std_logic_vector(2 downto 0)
-          encerrando: out std_logic);
+		E:  in std_logic_vector(2 downto 0);
+         	S: out std_logic_vector(2 downto 0);
+          	encerrando: out std_logic);
 end MAQUINA_ESTADOS;
 
 architecture ckt of MAQUINA_ESTADOS is
 
-  COMPONENT ffd is
-   port (clk, clr, set, d : in  std_logic;
+component ffd is
+   port (ck, clr, set, d : in  std_logic;
                        q : out std_logic);
-  end COMPONENT;
+end component;
 
-  signal clear: std_logic;
-  signal saida_ffd: std_logic_vector(2 downto 0);
+  	signal saida_ffd: std_logic_vector(2 downto 0);
+	signal setFF0,setFF1,setFF2: std_logic;
+	signal clearFF0,clearFF1,clearFF2: std_logic;
   
   begin
-	  F2: flipflopd port map(cLk, clear, '0', E(2), saida_ffd(2));
-	  F1: flipflopd port map(cLk, clear, '0', E(1), saida_ffd(1));
-	  F0: flipflopd port map(cLk, FAILURE, SUCCESS, E(0), saida_ffd(0));
+	
+	
+	setFF2 <= '1';
+	setFF1 <= '1';
+	setFF0 <= not success and failure;
+	
+	clearFF2 <= not success and not failure;
+	clearFF1 <= not success and not failure;
+	clearFF0 <=  not failure;
+	
+		
+
+	  F2: ffd port map(cLk, clearFF2,setFF2, E(2), saida_ffd(2));
+	  F1: ffd port map(cLk, clearFF1,setFF1, E(1), saida_ffd(1));
+	  F0: ffd port map(cLk, clearFF0,setFF0, E(0), saida_ffd(0));
     
-    clear <= FAILURE and SUCCESS;
+
     S(2) <= saida_ffd(2);
     S(1) <= saida_ffd(1);
     S(0) <= saida_ffd(0);
-    encerrando <= (saida_ffd(0) and not(saida_ffd(1)) and saida_ffd(2)) or 
-                  (not(saida_ffd(0)) and not(saida_ffd(1)) and not(saida_ffd(2)));
+
+    encerrando <= (saida_ffd(0) and not(saida_ffd(1)) and saida_ffd(2)) or (not(saida_ffd(0)) and not(saida_ffd(1)) and not(saida_ffd(2)));
 
 end ckt;
